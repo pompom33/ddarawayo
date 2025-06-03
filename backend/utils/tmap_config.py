1219b6997179bf6c0f04f346ed4a274e5134d9e6
pompom33 <yuna.isback33@gmail.com>
2025-06-03 17:22:19 +0900

    FEAT(utils): tmap_config에TMAP_REQUEST_URL 읽기 로직 추가, tmap_request에 build_payload 작성

--- backend/utils/tmap_config.py
+++ backend/utils/tmap_config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv() # .env 파일을 자동으로 찾아 읽어옴
 
-# TMAP_API_KEY = os.getenv("TMAP_API_KEY") # 실제 배포용
-TMAP_API_KEY = os.getenv("TMAP_API_KEY", "FAKE_TMAP_KEY_FOR_TEST") # 개발용
+TMAP_API_KEY = os.getenv("TMAP_API_KEY")
+TMAP_REQUEST_URL = os.getenv("TMAP_REQUEST_URL")
 
 if not TMAP_API_KEY:
-    raise ValueError("필수 API 키가 .env 파일에 없습니다!")
+    raise ValueError("TMAP_API_KEY가 .env 파일에 없습니다!")
+
+if not TMAP_REQUEST_URL:
+    raise ValueError("TMAP_REQUEST_URL가 .env 파일에 없습니다!")
--- backend/utils/tmap_request.py
+++ backend/utils/tmap_request.py
@@ -1,37 +1,61 @@
-import os
-from dotenv import load_dotenv
+from datetime import datetime
 import requests
 
-load_dotenv() # .env 파일을 자동으로 찾아 읽어옴
+from backend.models import model_input
+from tmap_config import TMAP_API_KEY, TMAP_REQUEST_URL
 
-TMAP_API_KEY = os.getenv("TMAP_API_KEY")
-TMAP_REQUEST_URL = os.getenv("TMAP_REQUEST_URL")
 
-# endpoint (여기로 request 보냄)
-url = TMAP_REQUEST_URL
+def load_startTime():
+    start_dt = model_input.load_timedelta()
+    dt = datetime.strptime(f"{start_dt}", '%Y-%m-%d %H:%M:%S.%f%z')
+    start_time = dt.strftime('%Y%m%d%H')
+    return start_time
 
-def build_payload():
+def build_payload(start_time, via_points):
     payload = {
         "reqCoordType": "WGS84GEO",
         "resCoordType": "WGS84GEO",
-        "startName": "%EC%B6%9C%EB%B0%9C",
-        "startX": "126.977613738705",
-        "startY": "37.56523875839218",
-        "startTime": "201607010900",
-        "endName": "%EB%8F%84%EC%B0%A9",
-        "endX": "127.12668555134137",
-        "endY": "37.42007356038663",
-        "searchOption": "0",
-        "carType": "4",
-        "truckType": "1",
+        "startName": "%EC%B6%9C%EB%B0%9C%EC%84%BC%ED%84%B0", # 출발센터
+        "startX": "127.0717955",
+        "startY": "37.4957886",
+        "startTime": start_time,
+        "endName": "%EB%8F%84%EC%B0%A9%EC%84%BC%ED%84%B0", #도착센터
+        "endX": "127.0717955",
+        "endY": "37.4957886",
+        "searchOption": "0", # 경로 탐색 옵션: 교통최적+추천 / 누리님은 "2"로 했었음
+        "carType": "4", # 대형화물차
+        "truckType": "1", # 화물자동차
         "truckWidth": "250",
         "truckHeight": "340",
         "truckWeight": "35500",
         "truckTotalWeight": "26000",
-        "truckLength": "880"
-        # "viaPoints": []
+        "truckLength": "880",
+        "viaPoints": via_points
     }
     return payload
+
+def mock_move_info():
+    move_info = [1, 2, 3]
+    return move_info
+
+
+def build_viaPoints(move_info):
+    via_points = []
+    for i in range(len(move_info)):
+        via_point = {} # 초기화
+
+        via_point["viaPointId"] = viaPointId
+        via_point["viaPointName"] = viaPointName
+        via_point["viaX"] = viaX
+        via_point["viaY"] = viaY
+        via_point["viaDetailAddress"] = viaDetailAddress
+        via_point["viaTime"] = viaTime
+        via_point["wishStartTime"] = wishStartTime
+        via_point["wishEndTime"] = wishEndTime
+
+        via_points.append(via_point)
+    return via_points
+
 
 def build_headers():
     headers = {
@@ -43,10 +67,21 @@ def build_headers():
 
 
 if __name__ == "__main__":
-    payload = build_payload()
-    headers = build_headers()
-    print(url)
-    print("repr of key:", repr(TMAP_API_KEY))
-    response = requests.post(url, json=payload, headers=headers)
+    # 테스트용 데이터
+    viaPointId= "test01"
+    viaPointName= "name01"
+    viaX= "126.95042955033101"
+    viaY= "37.39952907832974"
+    viaDetailAddress= "2001동, 1001호"
+    viaTime= 600
+    wishStartTime= "201606301700"
+    wishEndTime= "201606301900"
 
-    print(response.text)
+    start_time = load_startTime()
+    move_info = mock_move_info()
+    via_points = build_viaPoints(move_info)
+    payload = build_payload(start_time, via_points)
+    headers = build_headers()
+
+    print("\n----viapoints----")
+    print(via_points)
