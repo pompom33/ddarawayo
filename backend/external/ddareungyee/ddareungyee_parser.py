deaea7ef8d06729b31fdd0eff2ca9da18c0a7eef
pompom33 <yuna.isback33@gmail.com>
2025-06-11 11:28:24 +0900

    feat(external/ddareungyee): 실시간 재고량 parser 구현

--- backend/external/ddareungyee/ddareungyee_parser.py
+++ backend/external/ddareungyee/ddareungyee_parser.py
@@ -1,80 +1,26 @@
-import requests
+from ddareungyee_api import fetch_ddareungyee_stock
 
-from ddareungyee_config import build_ddareungyee_url
-from backend.data_loader import zone_info
+# 1. 실시간 대여 가능 자전거 수 (관리 권역 선별X)
+def parse_stock(start, end):
+    response = fetch_ddareungyee_stock(start, end)
+    items = response['rentBikeStatus']['row']
 
-# 1. 실시간 대여 가능 자전거 수
-def load_stock(zone_id_list):
+    stock_dict = {}
+    for item in items:
+        stationId = item['stationId']
+        stock = item['parkingBikeTotCnt']
+        stock_dict[stationId] = stock
 
-
-    # user input 시간만 stock 불러옴
-    input_date = datetime(2023, month, day)
-    input_date = str(input_date.strftime('%Y-%m-%d'))
-    input_time = int(hour)
-
-    # Bigquery에서 해당 기간 stock 내역 불러옴
-    # stock_list = []
-    # query = """
-    #     SELECT *
-    #     FROM `multi-final-project.Final_table_NURI.2023_available_stocks_fin`
-    #     WHERE Date = '{input_date}'
-    #         AND Time = {input_time}
-    #         AND Rental_location_ID IN {zone_id_tuple}
-    # """.format(
-    #     input_date=input_date,
-    #     input_time=input_time,
-    #     zone_id_tuple=zone_id_tuple
-    # )
-    # query_job = client.query(query)
-    # results = query_job.result()
-
-    for row in results:
-        stock_list.append(dict(row))
-
-    return stock_list  # 순서 X
-
+    return stock_dict
 
 
 if __name__ == '__main__':
-    url = build_ddareungyee_url(1, 5)
-    request = requests.get(url)
-    response = request.json()
+    start = 1001
+    end = 3000
+    cnt = 1
 
-    rows = response['rentBikeStatus']['row']
-    for row in rows:
-        for key, value in row.items():
-            print(f"  {key}: {value}")
-        print()
-
-    zone = 'zone1'
-    zone_id_list = zone_info.load_zone_id(zone)
-#
-#
-#
-# # 2. 대여소별 충분/부족 여부
-# def find_station_status(merged_result): #abundant, deficient labeling 하기
-#     # 1. 순서대로 status 구하기
-#     for stationid, item in merged_result.items():
-#         stock = item["stock"]
-#         predicted_rental = item['predicted_rental']
-#         status = stock - (predicted_rental + 3) # 예측된 수요량보다 3개 더 많아야 함
-#         if status < 0:
-#             merged_result[stationid]["status"] = 1
-#         else:
-#             merged_result[stationid]["status"] = "abundant"
-#
-#     # 2. 임의로 center 정보 추가
-#     merged_result["center"] = {
-#             "predicted_rental": 0,
-#             "stock": 0,
-#             "status": "abundant"
-#         }
-#     station_status_dict = merged_result
-#     session['station_status_dict'] = station_status_dict
-#
-#     return station_status_dict # center 까지 포함 (필수) # 순서 X
-#
-#
-# if __name__ == '__main__':
-#     zone = 'zone1'
-#     zone_id_list = zone_info .load_zone_id(zone)
+    for i in range(start, end, 500): #500은 청크 단위
+        stock_dict = parse_stock(i, i+499)
+        for key, value in stock_dict.items():
+            print(f"{cnt} - {key}: {value}")
+            cnt += 1
